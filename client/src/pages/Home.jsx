85ddac6b0e814b50b7e350a31620b4c2732f7472
RhettRoseman <rroseman@rcplanners.com>
2023-12-05 09:17:21 -0500

    header and footer text

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,24 @@
 import { useQuery } from '@apollo/client';
 
-import ThoughtList from '../components/ThoughtList';
-import ThoughtForm from '../components/ThoughtForm';
+// import ThoughtList from '../components/ThoughtList';
+// import ThoughtForm from '../components/ThoughtForm';
 
 import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  const thoughts = data?.thoughts || [];
+  // const { loading, data } = useQuery(QUERY_THOUGHTS);
+  // const thoughts = data?.thoughts || [];
 
   return (
     <main>
       <div className="flex-row justify-center">
-        <div
+        {/* <div
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}
         >
           <ThoughtForm />
-        </div>
-        <div className="col-12 col-md-8 mb-3">
+        </div> */}
+        {/* <div className="col-12 col-md-8 mb-3">
           {loading ? (
             <div>Loading...</div>
           ) : (
@@ -27,7 +27,7 @@ const Home = () => {
               title="Some Feed for Thought(s)..."
             />
           )}
-        </div>
+        </div> */}
       </div>
     </main>
   );
