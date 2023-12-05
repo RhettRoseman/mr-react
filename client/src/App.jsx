940359bebe12fe780bd05549211421cd907e9bef
Matt Estin <mattestin@gmail.com>
2023-12-05 14:49:10 -0500

    added header & header.css files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Projects from './components/Projects';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -14,11 +15,28 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="flex-column justify-flex-start min-100-vh">
+
+        {/*Header Section*/}
         <Header />
+
+
+
+        {/*Section 1*/}
+        <Projects />
+
+
+
+        {/*Section 2*/}
         <div className="container">
           <Outlet />
         </div>
-    
+
+
+
+        {/*Section 3*/}
+
+
+        {/*Footer Section*/}
         <Footer />
       </div>
     </ApolloProvider>
--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -2,8 +2,24 @@ import { Link } from 'react-router-dom';
 import "../../assets/css/style.css"
 const Header = () => {
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
+    <header>
+        <nav className="rose-nav ml-auto">
+            <div><h1>ROSE Technologies</h1></div>
+
+            <div>
+                <ul>
+                    <li>
+                        <a href="/home">Home</a></li>
+                    <li><a href="/blog">Blog</a>
+                    </li>
+                    <li><a href="/portfolio">Portfolio</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+
+            </div>
+        </nav>
+      <div className="container flex-row justify-space-between-lg justify-center align-center ">
         <Link className="text-light" to="/">
 
         </Link>
@@ -11,22 +27,7 @@ const Header = () => {
 
 
           <div className="flex-column justify-flex-start min-100-vh">
-              <nav className="rose-nav ml-auto">
-                  <div><p>ROSE Technologies</p></div>
 
-                  <div>
-                      <ul>
-                          <li>
-                          <a href="/home">Home</a></li>
-                          <li><a href="/home">Blog</a>
-                          </li>
-                          <li><a href="/home">Portfolio</a></li>
-                          <li><a href="/home">About</a></li>
-                          <li><a href="/home">Contact</a></li>
-                      </ul>
-
-                  </div>
-                </nav>
               <div className="container"><main><div className="flex-row justify-center"></div></main></div>
               <footer className="w-100 mt-auto bg-secondary p-4"><div className="container text-center mb-5"><p>  ROSE Technologies™ 2023 </p></div>
               </footer></div>
