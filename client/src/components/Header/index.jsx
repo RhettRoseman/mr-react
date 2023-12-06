949222dce9b14e84960afd19333b33134a432918
Matt Estin <mattestin@gmail.com>
2023-12-06 14:33:59 -0500

    edit to bug in header.js

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,68 +1,81 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "../../assets/css/style.css"
-import '../css/header.css'
+import '../../assets/css/style.css';
+import '../css/header.css';
+
 const Header = () => {
-  return (
+    return (
+        <header>
+            <nav className="rose-nav ml-auto">
+                <div>
+                    <h1>ROSE Technologies</h1>
+                </div>
 
-    <header>
-        <nav className="rose-nav ml-auto">
-            <div><h1>ROSE Technologies</h1></div>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/home">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/blog">Blog</Link>
+                        </li>
+                        <li>
+                            <Link to="/portfolio">Portfolio</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/team">Our Team</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                        <li>
+                            <Link to="/services">Services</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
 
-            <div>
-                <ul>
-                    <li>
-                        {/*<a href="/home">Home</a>*/}
-                    <Link to="/home">Home</Link>
-                    </li>
-                    <li><a href="/blog">Blog</a>
-                    </li>
-                    <li><a href="/portfolio">Portfolio</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/team">Our Team</a> </li>
-                    <li><a href="/contact">Contact</a></li>
-                    <li><a href="/services">Services</a></li>
-                </ul>
+            <div className="container flex-row justify-space-between-lg justify-center align-center">
+                <nav className="rose-nav ml-auto">
+                    <div className="titleimage"></div>
+                    <div>
+                        <ul>
+                            <li>
+                                <Link to="/home">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/blog">Blog</Link>
+                            </li>
+                            <li>
+                                <Link to="/portfolio">Portfolio</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
 
+                <div className="container flex-row justify-space-between-lg justify-center align-center">
+                    <Link className="text-light" to="/"></Link>
+
+                    <div className="flex-column justify-flex-start min-100-vh">
+                        <div className="container">
+                            <main>
+                                <div className="flex-row justify-center"></div>
+                            </main>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </nav>
-      <div className="container flex-row justify-space-between-lg justify-center align-center ">
-
-    <div>
-           <nav className="rose-nav ml-auto">
-     
-     <div className='titleimage'></div>
-
-     <div>
-     <ul>
-             <li>
-             <a href="/home">Home</a></li>
-             <li><a href="/Blog">Blog</a>
-             </li>
-             <li><a href="/Portfolio">Portfolio</a></li>
-             <li><a href="/About">About</a></li>
-             <li><a href="/Contact">Contact</a></li>
-         </ul>
-
-     </div>
-   </nav>
-    <header className=" header">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-
-        <Link className="text-light" to="/">
-
-        </Link>
-
-
-
-          <div className="flex-column justify-flex-start min-100-vh">
-
-              <div className="container"><main><div className="flex-row justify-center"></div></main></div>
-
-              </div>
-       </div>
-    </header>
-  );
+        </header>
+    );
 };
 
 export default Header;
