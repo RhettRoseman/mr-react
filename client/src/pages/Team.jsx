baa14bd4211f905148602ce3c2b35fadeab0ba80
spencer1227 <hoile1227@gmail.com>
2023-12-06 17:23:33 -0600

    update css

--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -14,7 +14,7 @@ return (
         <h3>(Title)</h3>
         <div className="info">
             {/* <a href="tel:+1800229933">Call us free!</a> */}
-            <a id="tag" target="_blank" href="tel:+17043493390"><img src="https://i.pinimg.com/564x/8e/a0/83/8ea08365d331d8ae9bd93ba5f50528d4.jpg"></img></a>
+            <a id="tag" target="_blank" href="tel:+17043493390"><img src="https://iaccessibility.net/wp-content/uploads/2016/09/2013-08-26_09-38-25__Phone_iOS7_App_Icon_Rounded.png"></img></a>
             <a id="tag" target="_blank" href="mailto:rhettroseman1@gmail.com"><img src="https://play-lh.googleusercontent.com/KSuaRLiI_FlDP8cM4MzJ23ml3og5Hxb9AapaGTMZ2GgR103mvJ3AAnoOFz1yheeQBBI=w240-h480-rw"></img></a>
             <a id="tag" target="_blank" href="https://github.com/RhettRoseman"><img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"></img></a>
             <a id="tag" target="_blank" href=""><img src="https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw=w240-h480-rw"></img></a>
@@ -24,7 +24,7 @@ return (
         <h2>Matt Estin</h2>
         <h3>(Title)</h3>
         <div className="info">
-            <a id="tag" target="_blank" href="tel:+12769718343"><img src="https://i.pinimg.com/564x/8e/a0/83/8ea08365d331d8ae9bd93ba5f50528d4.jpg"></img></a>
+            <a id="tag" target="_blank" href="tel:+12769718343"><img src="https://iaccessibility.net/wp-content/uploads/2016/09/2013-08-26_09-38-25__Phone_iOS7_App_Icon_Rounded.png"></img></a>
             <a id="tag" target="_blank" href="mailto:mattestin@gmail.com"><img src="https://play-lh.googleusercontent.com/KSuaRLiI_FlDP8cM4MzJ23ml3og5Hxb9AapaGTMZ2GgR103mvJ3AAnoOFz1yheeQBBI=w240-h480-rw"></img></a>
             <a id="tag" target="_blank" href="https://github.com/mattestinb"><img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"></img></a>
             <a id="tag" target="_blank" href=""><img src="https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw=w240-h480-rw"></img></a>
@@ -34,7 +34,7 @@ return (
         <h2>Sunaina Ojha</h2>
         <h3>(Title)</h3>
         <div className="info">
-            <a id="tag" target="_blank" href=""><img src="https://i.pinimg.com/564x/8e/a0/83/8ea08365d331d8ae9bd93ba5f50528d4.jpg"></img></a>
+            <a id="tag" target="_blank" href=""><img src="https://iaccessibility.net/wp-content/uploads/2016/09/2013-08-26_09-38-25__Phone_iOS7_App_Icon_Rounded.png"></img></a>
             <a id="tag" target="_blank" href=""><img src="https://play-lh.googleusercontent.com/KSuaRLiI_FlDP8cM4MzJ23ml3og5Hxb9AapaGTMZ2GgR103mvJ3AAnoOFz1yheeQBBI=w240-h480-rw"></img></a>
             <a id="tag" target="_blank" href="https://github.com/sunainaojha"><img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"></img></a>
             <a id="tag" target="_blank" href=""><img src="https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw=w240-h480-rw"></img></a>
@@ -44,7 +44,7 @@ return (
         <h2>Spencer Hoile</h2>
         <h3>(Title)</h3>
         <div className="info">
-            <a id="tag" target="_blank" href="tel:+12624433948"><img src="https://i.pinimg.com/564x/8e/a0/83/8ea08365d331d8ae9bd93ba5f50528d4.jpg"></img></a>
+            <a id="tag" target="_blank" href="tel:+12624433948"><img src="https://iaccessibility.net/wp-content/uploads/2016/09/2013-08-26_09-38-25__Phone_iOS7_App_Icon_Rounded.png"></img></a>
             <a id="tag" target="_blank" href="mailto:hoile1227@yahoo.com"><img src="https://play-lh.googleusercontent.com/KSuaRLiI_FlDP8cM4MzJ23ml3og5Hxb9AapaGTMZ2GgR103mvJ3AAnoOFz1yheeQBBI=w240-h480-rw"></img></a>
             <a id="tag" target="_blank" href="https://github.com/spencer1227"><img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"></img></a>
             <a id="tag" target="_blank" href="https://www.linkedin.com/in/spencerhoile"><img src="https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw=w240-h480-rw"></img></a>
