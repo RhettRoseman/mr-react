168bc8c9e648ba284fd309c5ca4605ec284939cc
RhettRoseman <rroseman@rcplanners.com>
2023-12-06 17:04:17 -0500

    debugging

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-import App from './App.jsx'
-import Home from './pages/Home'
-// import SingleThought from './pages/SingleThought'
-import Error from './pages/Error'
-import About from './pages/About.jsx'
-import Team from './pages/Team.jsx'
-<<<<<<< HEAD
-import { element } from 'prop-types'
-=======
-import Contact from './pages/Contact.jsx'
-import Portfolio from './pages/Portfolio.jsx'
-import Services from './pages/Services.jsx'
->>>>>>> main
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import About from './pages/About.jsx';
+import Team from './pages/Team.jsx';
+import Contact from './pages/Contact.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Services from './pages/Services.jsx';
 
 const router = createBrowserRouter([
   {
@@ -25,46 +19,31 @@ const router = createBrowserRouter([
         index: true,
         element: <Home />,
       },
-     {
-      path: '/about',
-      element: <About/>,
-     },
-     {
-<<<<<<< HEAD
-      path: '/blog',
-      element: <Team/>,
-     },
-     {
-      path: '/Contact',
-      element: <Contact/>,
-
-     },
       {
-        path: '/thoughts/:thoughtId',
-        element: <SingleThought />,
+        path: '/about',
+        element: <About />,
       },
-=======
-      path: '/team',
-      element: <Team/>,
-     },
-   
-        {
-          path: '/Contact',
-          element: <Contact />,
-        },
-        {
-          path: '/Portfolio',
-          element: <Portfolio />,
-        },
-        {
-          path: '/Services',
-          element: <Services />,
-        }
->>>>>>> main
+      {
+        path: '/team',
+        element: <Team />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/services',
+        element: <Services />,
+      },
+      // Add more routes as needed
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
-)
+);
