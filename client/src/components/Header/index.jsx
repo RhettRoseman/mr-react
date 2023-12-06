c223fc727f67e395f35640767c7c7e7a529d196a
RhettRoseman <rroseman@rcplanners.com>
2023-12-06 13:40:36 -0500

    Aboutpage styled

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -22,20 +22,16 @@ const Header = () => {
      </div>
    </nav>
     <header className=" header">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
+      
         <Link className="text-light" to="/">
 
         </Link>
 
 
 
-          <div className="flex-column justify-flex-start min-100-vh">
-             
-           
-              {/* <footer className="w-100 mt-auto bg-secondary p-4"><div className="container text-center mb-5"><p>  ROSE Technologies™ 2023 </p></div>
-              </footer></div> */}
-       </div>
-       </div>
+
+    
+      
     </header>
 
    </div>
