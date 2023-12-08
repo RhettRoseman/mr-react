67aff1db59816b81bd7ba743229fe4725b1b0a77
RhettRoseman <rroseman@rcplanners.com>
2023-12-08 11:14:29 -0500

    services div are clickable and go to options route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,6 +8,7 @@ import Team from './pages/Team.jsx';
 import Contact from './pages/Contact.jsx';
 import Portfolio from './pages/Portfolio.jsx';
 import Services from './pages/Services.jsx';
+import Options from './pages/option.jsx';
 
 const router = createBrowserRouter([
   {
@@ -44,7 +45,10 @@ const router = createBrowserRouter([
         path: '/services',
         element: <Services />,
       },
-  
+      {
+        path: '/options',
+        element: <Options/>
+      }
     ],
   },
 ]);
---
+++ client/src/pages/Option.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Options(){
+
+return (
+    <div>Form placement</div>
+)
+
+
+
+}
